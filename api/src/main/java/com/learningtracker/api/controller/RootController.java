c7b48345c5c330de13d92f94feaf07f86d439616
Vikash Saini <vikashs@steepgraph.com>
2025-11-17 14:46:08 +0530

    Deploying FE+BE together on prod

---
+++ api/src/main/java/com/learningtracker/api/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.learningtracker.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend running OK";
+    }
+}
+
